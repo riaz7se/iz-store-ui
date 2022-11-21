de830fddce90a9d23d149a4962689860af3ef9df
Riaz 7SE <riazsyed11@gmail.com>
2022-11-21 07:37:01 +0530

    Persist Store

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import persistStore from 'redux-persist/es/persistStore'
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App'
 import './index.css'
 import store from './store'
 
+let persistor = persistStore(store)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store = {store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
     
   </React.StrictMode>
--- src/store.js
+++ src/store.js
@@ -1,14 +1,58 @@
-import { configureStore } from "@reduxjs/toolkit";
-
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
 import cartReducer from "./features/cart/cartSlice";
 import myStoreReducer from "./features/items/myStoreSlice";
+import thunk from 'redux-thunk';
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
 
 
-const store = configureStore({
-    reducer: {
-        storeItems: myStoreReducer,
-        cart: cartReducer
-    }
-});
+// const persistConfig = {
+//     key: 'root',
+//     storage
+// }
 
-export default store;
+// const persistedCartReducer = persistReducer(persistConfig, cartReducer);
+
+// const store = configureStore({
+//     reducer: {
+//         storeItems: myStoreReducer,
+//         cart: persistedCartReducer,
+//     },
+//     middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//         serializableCheck: {
+//             ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//         },
+//     }),
+// });
+
+
+
+/// other method to persist
+const reducers = combineReducers({
+    storeItems: myStoreReducer,
+    cart: cartReducer,
+  });
+  
+  const persistConfig = {
+    key: 'root',
+    storage,
+  };
+  
+  const persistedReducer = persistReducer(persistConfig, reducers);
+  
+  const store = configureStore({
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: [thunk],
+  });
+
+  export default store;
